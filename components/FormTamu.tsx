260945db2550afbe744de7be6b6edf3247b39f84
Mukhtar Muslim <mukhtartheacanva@gmail.com>
2026-05-04 19:14:34 +0700

    update input tanggal di form tamu

--- components/FormTamu.tsx
+++ components/FormTamu.tsx
@@ -57,6 +57,12 @@ export default function FormTamu() {
         <textarea name="keperluan" required className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none text-black h-28" placeholder="Jelaskan tujuan kunjungan Anda..." />
       </div>
 
+      {/* Baris 6: tanggal berkunjung */}
+      <div>
+        <label className="block text-sm font-medium text-gray-700 mb-1">Tanggal Kunjungan</label>
+        <input name="tanggalBerkunjung" type="date" required className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none text-black"/>
+      </div>
+
       {/* Tombol Simpan */}
       <button 
         type="submit" 
